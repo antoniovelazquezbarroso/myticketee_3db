5ffd0f8f2fac8242fb8fa82ee4619f873603447c
antonio_velazquez <antonio.velazquezbarroso@gmail.com>
2016-02-09 18:38:14 +0100

    Add ability for admins to edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,12 @@
 class Admin::UsersController < Admin::ApplicationController
-  
+	
+  before_action :set_user, only: [:show, :edit, :update, :archive]
+
   def index
   	@users = User.order(:email)
+  end
+
+  def show
   end
 
   def new
@@ -20,6 +25,22 @@ class Admin::UsersController < Admin::ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been updated."
+      render "edit"
+    end
+  end
 
 	private
 
@@ -27,7 +48,9 @@ class Admin::UsersController < Admin::ApplicationController
     params.require(:user).permit(:email, :password, :admin)
   end
 
-
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 
 end
