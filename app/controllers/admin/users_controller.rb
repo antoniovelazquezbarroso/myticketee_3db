5da3b27d80ef56d614b98a1594dd30118d706eec
antonio_velazquez <antonio.velazquezbarroso@gmail.com>
2016-02-14 21:00:45 +0100

    Make projects only visible to users with permission to see them

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -43,14 +43,12 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
   def archive
- 
     if @user == current_user
       flash[:alert] = "You cannot archive yourself!"
     else
       @user.archive
       flash[:notice] = "User has been archived."
     end
-
     redirect_to admin_users_path
   end  
 
